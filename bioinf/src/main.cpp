5976832eb6f9a8ca4a78ac52b5ffc351094a5f3d
fpopic <filip.popic@gmail.com>
2016-12-17 02:23:16 +0100

    Sredjen README i libovi

--- bioinf/src/main.cpp
+++ bioinf/src/main.cpp
@@ -1,16 +1,7 @@
 #include <iostream>
-#include "mylib/MyIOLibrary.h"
 
 using namespace std;
 
 int main() {
-    MyIOLibrary myIOLibrary;
-    myIOLibrary.fileToCin("res/input.txt");
-
-    int x;
-    for (int i = 0; i < 4; ++i) {
-        cin >> x;
-        cout << x << endl;
-    }
     return 0;
 }
